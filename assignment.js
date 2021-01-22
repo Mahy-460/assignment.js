8db3f73c7dafd6e2c1d7c1dcbbe114034256c437
mahy-460 <imtiushasan420@gmail.com>
2021-01-22 22:14:39 +0600

    modifai This code

--- assignment.js
+++ assignment.js
@@ -19,27 +19,27 @@ console.log(result);
 
 
 
-// function budgetCalculator(num1,num2,num3){
+function budgetCalculator(num1,num2,num3){
  
-//      if(num1){
-//          var watch = num1 * 50;     // watch
-//          var add1 = watch;
-//      }
-//      if(num2){                      // phone
-//          var phone = num2 * 100;
-//          var add2 = phone;
-//          var add3 = add1 + add2;
-//      }
-//      if(num3){
-//          var laptop = num3 * 500;   // laptop
-//          var add3= laptop;
-//          var total = add1 + add2+ add3;
-//      }
-//      return total;
-// }
+     if(num1){
+         var watch = num1 * 50;     // watch
+         var add1 = watch;
+     }
+     if(num2){                      // phone
+         var phone = num2 * 100;
+         var add2 = phone;
+         var add3 = add1 + add2;
+     }
+     if(num3){
+         var laptop = num3 * 500;   // laptop
+         var add3= laptop;
+         var total = add1 + add2+ add3;
+     }
+     return total;
+}
 
-// var result = budgetCalculator(2,2,2);
-// console.log(result);
+var result = budgetCalculator(2,2,2);
+console.log(result);
 
 
 
@@ -48,47 +48,50 @@ console.log(result);
 
 /*------------------------------hotelCost-------------------------*/
 
-// function hotelCost(days){
-//     var day = 0;
-//     if(!(days > 0)){
-//         return "undifind";
-//     }
-//     if(days <= 10){
-//         day = days * 100;
-//     }
-//     else if(days <= 20){
-//         var firstPart = 10 * 100;
-//         var reming = days - 10;
-//         var secondPart = reming * 80;
-//         day = firstPart +  secondPart;
-//     }
-//     else{
-//         var firstPart = 10 * 100;
-//         var secondPart = 10 * 80;
-//         var reming = days - 20;
-//         var thardPart = reming * 50;
-//         day = firstPart + secondPart + thardPart;
-//     }
-//    return day
-// }
+function hotelCost(days){
+    var day = 0;
+    if(!(days > 0)){
+        return "undifind";
+    }
+    if(days <= 10){
+        day = days * 100;
+    }
+    else if(days <= 20){
+        var firstPart = 10 * 100;
+        var reming = days - 10;
+        var secondPart = reming * 80;
+        day = firstPart +  secondPart;
+    }
+    else{
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var reming = days - 20;
+        var thardPart = reming * 50;
+        day = firstPart + secondPart + thardPart;
+    }
+   return day
+}
 
-// var result = hotelCost(11);
-// console.log(result);
+var result = hotelCost(21);
+console.log(result);
 
-/*-----------------------------------------MegaFrind--------------------------*/
+/*-----------------------------------------megaFrind--------------------------*/
 
-// function MegaFrind(frinds){
-//     var max = [0];
-// for(var i = 0; i<frinds.length;i++){
-//     var element = frinds[i];
-//     if(max.length < frinds[i].length){
-//         max = frinds[i];
-//     }
+function megaFrind(frinds){
+    var max = [0];
+for(var i = 0; i<frinds.length;i++){
+    var element = frinds[i];
+    if(max.length < frinds[i].length){
+        max = frinds[i];
+    }
  
-// }
-// return max;
-// }
+}
+return max;
+}
 
-// var frinds = [];
-// var result  = MegaFrind(frinds);
-// console.log(result);
+var frinds = ["mahy Hassan ","laboni","albarte"];
+var result  = megaFrind(frinds);
+console.log(result);
+
+
+/*----------------------------------------------Finished-------------------------*/
